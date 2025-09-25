81668e919bf6f6af0f6a0e4538803e8135ae7697
Akshat Goel <121927473+Akshat-hype@users.noreply.github.com>
2025-09-25 12:23:54 +0530

    Create 3429-SpecialArrayI

---
+++ 3429-SpecialArrayI/3429-SpecialArrayI.java
@@ -0,0 +1,14 @@
+// Last updated: 9/25/2025, 12:23:54 PM
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        int n = nums.length;
+        if (n==1) return true;
+        
+        for (int i=0;i<n-1;i++) {
+            if (nums[i]%2==nums[i+1]%2) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
