d0d32643fdade57cf6d65e811c6f65cc33c360e8
Akshat Goel <121927473+Akshat-hype@users.noreply.github.com>
2025-09-25 12:24:34 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,18 @@
+// Last updated: 9/25/2025, 12:24:34 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length - 1;
+        while(l <= h){
+            int mid = (l + h) / 2;
+            if(nums[mid] == target){
+                return mid;
+            } else if(nums[mid] < target){
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
