f2bd8124e1b29044dc56e61c28c7e6e91436fec7
Akshat Goel <121927473+Akshat-hype@users.noreply.github.com>
2025-09-25 12:25:07 +0530

    Create 345-ReverseVowelsOfAString

---
+++ 345-ReverseVowelsOfAString/345-ReverseVowelsOfAString.java
@@ -0,0 +1,38 @@
+// Last updated: 9/25/2025, 12:25:06 PM
+class Solution {
+    public static boolean isvowel(char x) 
+    { 
+        if (x == 'a' || x == 'e' || x == 'i' || 
+            x == 'o' || x == 'u' || x == 'A' || 
+            x == 'E' || x == 'I' || x == 'O' || x == 'U'){
+           return true;
+            }
+        else{
+            return false;
+        }
+    }
+    public String reverseVowels(String s) {
+        char[] chars=s.toCharArray();
+        int i=0;
+        int j=chars.length-1;
+        
+        while (i<j) {
+            if(isvowel(chars[i])==false) {
+                i++;
+            }
+           else if(isvowel(chars[j])==false) {
+                j--;
+            }
+            else{
+            char temp=chars[i];
+            chars[i]=chars[j];
+            chars[j]=temp;
+            i++;
+            j--;
+            }
+        }
+        
+        String st=new String(chars);
+        return st;
+    }
+}
